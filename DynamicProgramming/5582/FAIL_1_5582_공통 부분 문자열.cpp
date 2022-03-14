f4814b9705ad70d1d443b32662a15c783b1c70ec
choidaehan <mnnm0417@gmail.com>
2022-03-14 21:28:36 +0900

    2번째 시간초과

--- DynamicProgramming/5582/FAIL_1_5582_공통 부분 문자열.cpp
+++ DynamicProgramming/5582/FAIL_1_5582_공통 부분 문자열.cpp
@@ -5,32 +5,31 @@
 using namespace std;
 
 // 문자열 A와 B를 일일히 비교하면서 일치하는 만큼 CNT변수 증가. A와 B가 달라지는 순간 break 후 cnt값 갱신.. 100퍼 시간초과 각
-
+int dp[4000];
 string A,B;
 int main() {
     cin >> A >> B;
     int result = 0;
      for(int i = 0; i < A.size(); i++) {
-        //cout << "First A[i] is " << A[i] << endl;
         for(int j = 0; j < B.size(); j++) {
-            //cout << "First B[j] is " << B[j] << endl;
+            // int cnt = 1;
              if(A[i] == B[j]) {
-                int cnt = 1;  // 2번째 loop로 옮겨보기
-                int idx = 1;
-                 while(i+idx <= A.size() && j+idx <= B.size()) {
-                  //  cout << "i+idx is " << i+idx << " j+idx is " << j+idx << endl;
-                    if(A[i+idx] == B[j+idx]) {
-                       // cout << "A[i+idx] is " << A[i+idx] << " B[j+idx] is " << B[j+idx] << endl;
-                        cnt+=1;
-                    } else {
-                       // cout << "result is " << result << " cnt is " << cnt << endl;
-                        result = max(result, cnt);
-                        break;
-                    }
-                    idx+=1;
+                //  cout << "A[i] is " << A[i] << " B[j] is " << B[j] << endl;
+                 dp[j] = max(dp[j], 1);
+                 for(int k = 1; (i+k < A.size() && j+k < B.size()); k++) {
+                     if(A[i+k] == B[j+k]) {
+                        // cout << "A[i+k] is \"" << A[i+k] << "\" B[j+k] is \"" << B[i+k] << "\" (k) is \"" << k << "\"" << endl;
+                        // dp[j+k] = max(dp[j+k, k);
+                        dp[j+k] = max(dp[j+k], k+1);
+                     } else {
+                         break;
+                     }
                  }
              }
          }
+     }
+     for (int i = 0; i < B.size(); i++) {
+         result = max(dp[i], result);
      }
     cout << result << endl;
 }
