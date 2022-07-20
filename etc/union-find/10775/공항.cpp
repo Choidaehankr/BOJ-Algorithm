374a0e57c0295a53e802dd4bf327368cba3045a0
Daehan Choi <mnnm22@naver.com>
2022-07-20 22:21:33 +0900

    algorithm study

--- etc/union-find/10775/공항.cpp
+++ etc/union-find/10775/공항.cpp
@@ -8,6 +8,7 @@ using namespace std;
 int gates[100001];
 int answer = 0;
 
+// num의 부모 찾기
 int find(int num) {
 	if (num == gates[num]){
 		return num;
@@ -17,6 +18,7 @@ int find(int num) {
 	}
 }
 
+// a의 부모를 b로 변경
 void merge(int a, int b) {
 	a = find(a);
 	b = find(b);
@@ -35,9 +37,10 @@ int main() {
 	int num;
 	for (int i = 0; i < P; i++) {
 		cin >> num;
-		int parent = find(num);  // num의 부모: 다음에 도킹할 게이트 번호
+		int parent = find(num);  
 		if (parent != 0) {
 			// 1 <= g1 <= G 이기 때문에 더 작은수와 merge 해줌
+			// num의 부모 parent를 parent-1과 merge 함으로 다음에 num이 입력됐을 때, 도킹해야할 곳은 parent-1로 설정
 			merge(parent, parent - 1);
 			answer++;
 		}
